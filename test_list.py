aceb6b31be1ca74199ee6452881ddbce30cf9b78
Illia Kripaka <kripakaillya@gmail.com>
2021-02-19 16:55:09 +0200

    feat: add new test with list

--- test_list.py
+++ test_list.py
@@ -11,5 +11,8 @@ class TestListMethods(unittest.TestCase):
     def test_roman_tkalenko_2(self):
         self.assertEqual(2, 2)
 
+    def test_illia_kripaka_fi_94_2(self)
+        self.asserEqual((list(lambda x: x*2, [1,3,5]) == [1,3,5]), bool("abc"))
+
 if __name__ == '__main__':
     unittest.main()
