a05ad6fe1a784a3393c589b9beaee4b58f793193
Illia Kripaka <30872146+ikripaka@users.noreply.github.com>
2021-02-19 20:33:38 +0200

    fix: add space after function

--- test_list.py
+++ test_list.py
@@ -14,5 +14,6 @@ class TestListMethods(unittest.TestCase):
     def test_illia_kripaka_fi_94_2(self):
         self.assertEqual(2*[1, 3, 5], [1, 3, 5, 1, 3, 5])
 
+
 if __name__ == '__main__':
     unittest.main()
